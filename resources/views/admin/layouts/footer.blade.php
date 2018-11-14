b0a056b1ce55806ecd423516b4981d6b3ec75723
Bien Hoang <bien@deha-soft.com>
2018-11-14 11:05:03 +0700

    Add HTML for admin store

---
+++ resources/views/admin/layouts/footer.blade.php
@@ -0,0 +1,13 @@
+</div>
+<!-- Javascript -->
+<script src="{{asset('st-admin/assets/bundles/libscripts.bundle.js')}}"></script>
+<script src="{{asset('st-admin/assets/bundles/vendorscripts.bundle.js')}}"></script>
+<script src="{{asset('st-admin/assets/bundles/chartist.bundle.js')}}"></script>
+<script src="{{asset('st-admin/assets/bundles/knob.bundle.js')}}"></script>
+<script src="{{asset('st-admin/assets/bundles/flotscripts.bundle.js')}}"></script>
+<script src="{{asset('st-admin/assets/vendor/flot-charts/jquery.flot.selection.js')}}"></script>
+<script src="{{asset('st-admin/assets/bundles/mainscripts.bundle.js')}}"></script>
+<script src="{{asset('st-admin/assets/js/index.js')}}"></script>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('admin')->group(function () {
+	Route::get('/', 'Admin\HomeController@index');
+});
